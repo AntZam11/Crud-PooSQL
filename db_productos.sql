1f13d8d002fdac3ea9a056f37396a7eae118d28b
Antzam <asantosz1@miumg.edu.gt>
2022-04-17 21:31:38 -0600

    crudpoo_sql

---
+++ db_productos.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_productos
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `marcas`
+--
+
+DROP TABLE IF EXISTS `marcas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `marcas` (
+  `idmarca` smallint NOT NULL AUTO_INCREMENT,
+  `marca` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idmarca`),
+  KEY `idMarcas_idx` (`idmarca`,`marca`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marcas`
+--
+
+LOCK TABLES `marcas` WRITE;
+/*!40000 ALTER TABLE `marcas` DISABLE KEYS */;
+INSERT INTO `marcas` VALUES (1,'samsung'),(2,'LG'),(3,'huawei'),(4,'sony'),(5,'samsung'),(6,'nokia'),(7,'SONY'),(8,'LG');
+/*!40000 ALTER TABLE `marcas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `productos` (
+  `idProductos` int NOT NULL AUTO_INCREMENT,
+  `producto` varchar(50) DEFAULT NULL,
+  `idmarca` smallint DEFAULT NULL,
+  `descripcion` varchar(100) DEFAULT NULL,
+  `precio_costo` decimal(8,2) DEFAULT NULL,
+  `precio_venta` decimal(8,2) DEFAULT NULL,
+  `existencia` int DEFAULT NULL,
+  `fecha_ingreso` datetime DEFAULT NULL,
+  PRIMARY KEY (`idProductos`),
+  KEY `marcas_idx` (`idmarca`),
+  CONSTRAINT `marcas` FOREIGN KEY (`idmarca`) REFERENCES `marcas` (`idmarca`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (2,'telefono',1,'celular',1000.00,1500.00,10,'2021-12-11 00:00:00'),(4,'telefono',2,'celular',1000.00,1500.00,15,'2021-11-12 00:00:00'),(6,'telefono',3,'celular',1500.00,2000.00,10,'2021-10-11 00:00:00'),(8,'celular',6,'movil',1000.00,1500.00,50,'2021-10-11 00:00:00'),(9,'celular',7,'movil',1000.00,1500.00,50,'2020-11-10 00:00:00');
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-17 21:12:11
